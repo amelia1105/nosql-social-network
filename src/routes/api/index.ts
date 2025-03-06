5c45d0b3b3013eda01d10d4da81c52824e3ba8c6
Amelia <aebellanger@yahoo.com>
2025-03-05 20:27:21 -0500

    Add outline and README template

---
+++ src/routes/api/index.ts
@@ -0,0 +1,13 @@
+// UPDATE THIS CODE!!!!!!
+// 
+
+import { Router } from 'express';
+import { courseRouter } from './courseRoutes.js';
+import { studentRouter } from './studentRoutes.js';
+
+const router = Router();
+
+router.use('/courses', courseRouter);
+router.use('/students', studentRouter);
+
+export default router;
