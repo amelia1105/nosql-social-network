0c11448c41c2542d40b642446e8079808987674e
Amelia <aebellanger@yahoo.com>
2025-03-08 20:17:55 -0500

    Add user and thought seed data

--- src/seeds/data.ts
+++ src/seeds/data.ts
@@ -1,119 +1,77 @@
-// UPDATE THIS CODE!!!!!!
-// 
-// 
-
-const names = [
-  'Aaran',
-  'Aaren',
-  'Aarez',
-  'Aarman',
-  'Aaron',
-  'Aaron-James',
-  'Aarron',
-  'Aaryan',
-  'Aaryn',
-  'Aayan',
-  'Aazaan',
-  'Abaan',
-  'Abbas',
-  'Abdallah',
-  'Abdalroof',
-  'Abdihakim',
-  'Abdirahman',
-  'Abdisalam',
-  'Abdul',
-  'Abdul-Aziz',
-  'Abdulbasir',
-  'Abdulkadir',
-  'Abdulkarem',
-  'Smith',
-  'Jones',
-  'Coollastname',
-  'enter_name_here',
-  'Ze',
-  'Zechariah',
-  'Zeek',
-  'Zeeshan',
-  'Zeid',
-  'Zein',
-  'Zen',
-  'Zendel',
-  'Zenith',
-  'Zennon',
-  'Zeph',
-  'Zerah',
-  'Zhen',
-  'Zhi',
-  'Zhong',
-  'Zhuo',
-  'Zi',
-  'Zidane',
-  'Zijie',
-  'Zinedine',
-  'Zion',
-  'Zishan',
-  'Ziya',
-  'Ziyaan',
-  'Zohaib',
-  'Zohair',
-  'Zoubaeir',
-  'Zubair',
-  'Zubayr',
-  'Zuriel',
-  'Xander',
-  'Jared',
-  'Courtney',
-  'Gillian',
-  'Clark',
-  'Jared',
-  'Grace',
-  'Kelsey',
-  'Tamar',
-  'Alex',
-  'Mark',
-  'Tamar',
-  'Farish',
-  'Sarah',
-  'Nathaniel',
-  'Parker',
+const usernames = [
+'user123',
+'coolguy',
+'techgal',
+'coder123',
+'devwizard',
+'hackerqueen',
+'ninjaCoder',
+'proGamer',
+'alphaTester',
+'betaUser',
+'charlieDev',
+'deltaForce',
+'echoCoder',
+'foxtrotDev',
+'golfHacker',
+'hotelTech',
+'indiaCoder',
+'julietDev',
+'kiloHacker',
+'limaTech',
+'mikeCoder',
+'novemberDev',
+'oscarHacker',
+'papaTech',
+'quebecCoder',
+'romeoDev',
+'sierraHacker',
+'tangoTech',
+'uniformCoder',
+'victorDev',
+'whiskeyHacker',
+'xrayTech',
+'yankeeCoder',
+'zuluDev'
 ];
 
-const appDescriptions = [
-  'Decision Tracker',
-  'Find My Phone',
-  'Learn Piano',
-  'Starbase Defender',
-  'Tower Defense',
-  'Monopoly Money Manager',
-  'Movie trailers',
-  'Hello world',
-  'Stupid Social Media App',
-  'Notes',
-  'Messages',
-  'Email',
-  'Compass',
-  'Firefox',
-  'Running app',
-  'Cooking app',
-  'Poker',
-  'Deliveries',
+const thoughts = [
+  'I love coding!',
+  'I am a coding wizard!',
+  'JavaScript is awesome!',
+  'TypeScript makes coding so much better!',
+  'I enjoy solving complex problems.',
+  'Debugging is like being a detective.',
+  'Learning new programming languages is fun!',
+  'I love contributing to open source projects.',
+  'Coding challenges keep my skills sharp.',
+  'Pair programming is a great way to learn.',
+  'I enjoy building web applications.',
+  'Refactoring code makes it cleaner and more efficient.',
+  'Unit tests are essential for reliable code.',
+  'I love the feeling of solving a tough bug.',
+  'Code reviews help improve code quality.',
+  'Continuous integration is a game changer.',
+  'I enjoy working with APIs.',
+  'Version control with Git is a must.',
+  'I love automating repetitive tasks.',
+  'Writing clean code is important.',
+  'I enjoy learning about new technologies.',
+  'Coding is both an art and a science.',
 ];
 
 // Get a random item given an array
-export const getRandomArrItem = (arr: any) => arr[Math.floor(Math.random() * arr.length)];
+const getRandomArrItem = (arr: any) => arr[Math.floor(Math.random() * arr.length)];
 
-// Gets a random full name
-export const getRandomName =() =>
-  `${getRandomArrItem(names)} ${getRandomArrItem(names)}`;
+// Get a random username
+export const getRandomUsername =() =>
+  `${getRandomArrItem(usernames)} ${getRandomArrItem(usernames)}`;
 
-// Function to generate random assignments that we can add to student object.
-export const getRandomAssignments = (int: number) => {
+// Generate random thoughts that we can add to user object
+export const getRandomThoughts = (int: number) => {
   const results = [];
   for (let i = 0; i < int; i++) {
-    results.push({
-      name: getRandomArrItem(appDescriptions),
-      score: Math.floor(Math.random() * (99 - 70 + 1) + 70),
-    });
+    results.push(getRandomArrItem(thoughts));
   }
   return results;
 };
